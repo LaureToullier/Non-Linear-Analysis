542714303eea00ed90d88e00a7cd0897e477a7e5
s221568 <jeremie.engler@epfl.ch>
2023-12-06 14:28:41 +0100

    tests

--- Projet/functions_definitions.py
+++ Projet/functions_definitions.py
@@ -1,3 +1,53 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def csv_plot(file_path, file_name):
+
+    data = pd.read_csv(file_path).drop(pd.read_csv(file_path).columns[0], axis = 1)
+
+    deplacements = data['Axial_Disp. [mm]']
+    rotations = data['Rotation']
+    moments = data['Base moment [kN.m]']
+
+    plt.plot(rotations, moments, linestyle='-', linewidth=1, color='green', label='Moments vs Rotations')
+
+    # Ajouter des labels et un titre
+    plt.xlabel('Déplacements [mm]')
+    plt.ylabel('Moments [kN.m]')
+    plt.title(f'Relation entre Moments et Déplacements (Diagramme de lignes), file: {file_name}')
+    plt.legend()
+
+    # Afficher le graphique
+    plt.show()
+
+    return None
+
+
+
+def csv_plot_export(file_path, file_name):
+
+    data = pd.read_csv(file_path).drop(pd.read_csv(file_path).columns[0], axis = 1)
+
+    deplacements = data['Axial_Disp. [mm]']
+    rotations = data['Rotation']
+    moments = data['Base moment [kN.m]']
+
+    plt.plot(rotations, moments, linestyle='-', linewidth=1, color='green', label='Moments vs Rotations')
+
+    # Ajouter des labels et un titre
+    plt.xlabel('Déplacements [mm]')
+    plt.ylabel('Moments [kN.m]')
+    plt.title(f'Relation entre Moments et Déplacements (Diagramme de lignes), file: {file_name}')
+    plt.legend()
+
+    # Afficher le graphique
+    plt.show()
+
+    return None
+
+
 def printgraph(x,y,xname,yname,unit1, unit2):
     plt.figure(figsize=(7, 5))
     plt.plot(x,y,'-o', mfc="w")
@@ -14,4 +64,3 @@ def printgraph(x,y,xname,yname,unit1, unit2):
     plt.show()
 
     return None
-
